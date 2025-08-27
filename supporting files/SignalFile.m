effa922a80aa815ef216b30e4ceba17edc0bf1b6
silanglois <simonlanglois@u.boisestate.edu>
2025-08-26 18:12:39 -0600

    implemented the modifyBackground private method and related properties

--- supporting files/SignalFile.m
+++ supporting files/SignalFile.m
@@ -1,6 +1,10 @@
 classdef SignalFile < File
     properties
         bg File % file object for the corresponding background
+
+        bgSmoothFactor double = 0 % smoothing factor for the background
+        bgSlope        double = 1 % slope of the offset applied to the background
+        bgIntercept    double = 0 % intercept of the offset applied to the background
     end
 
     methods (Access = public)
@@ -13,13 +17,24 @@ classdef SignalFile < File
         end
 
         function subtract_background(obj)
-
             if ~isequal(obj.processed_data.Wavelength, obj.bg.processed_data.Wavelength)
                 error("Wavelength mismatch during background subtraction.");
             end
+            bgData = modifyBackground;
+            obj.processed_data.Intensity = obj.processed_data.Intensity - bgData.Intensity;
+        end
+    end
 
-            obj.processed_data.Intensity = ...
-                obj.processed_data.Intensity - obj.bg.processed_data.Intensity;
+    methods (Access = private)
+        function bgData = modifyBackground(obj)
+            bgData = obj.bg.processed_data;
+            if 0 <= obj.bgSmoothFactor && obj.bgSmoothFactor <= 1
+                bgData = smoothdata(bgData, "movmedian", ...
+                    SmoothingFactor=obj.bgSmoothFactor, DataVariables="Intensity");
+            else
+                error("Error: Smoothing factor for the background of %s has to be between 0 and 1", obj.filename)
+            end
+            bgData.Intensity = bgData.Intensity * obj.bgSlope + obj.bgIntercept;
         end
     end
 end
